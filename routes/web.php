70c4a04dd7f31b64413ff4680ef63985b72e9759
Chehine Ammari <chehine.ammari@gmail.com>
2024-01-18 16:18:32 +0100

    C2: add update+delete routes + implement update+destroy methods

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::patch('/{todo}', [TodoController::class,'update'])->name('update');
+Route::delete('/{todo}', [TodoController::class,'destroy'])->name('destroy');
