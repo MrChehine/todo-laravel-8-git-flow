8487dc005436de5b577d4212bef7f9d33fda5f2d
Chehine Ammari <55857626+MrChehine@users.noreply.github.com>
2024-01-18 16:54:57 +0100

    Merge: feat(show-todo) -> dev
    * C4: implement index method on TodoController
    
    * C5: Implement home page

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -9,7 +9,8 @@ class TodoController extends Controller
 {
     public function index()
     {
-
+        $todos = Todo::all();
+        return view("welcome", ["todos"=> $todos]);
     }
 
     public function store(Request $request)
