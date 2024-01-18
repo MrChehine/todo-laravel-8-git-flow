afc2a7cd8d69bd48bcdf0175cb144c9c8a4bc684
Chehine Ammari <chehine.ammari@gmail.com>
2024-01-18 16:10:06 +0100

    Init TodoController methods

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function update(Request $request, Todo $todo)
+    {
+
+    }
+
+    public function destroy(Todo $todo)
+    {
+
+    }
 }
